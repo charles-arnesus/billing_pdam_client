b6d7d0489b5a644964b9545be8446f1e89fc0a83
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 12:58:28 +0700

    [Charles] add initial spring boot project

---
+++ src/main/java/com/charles/billing_pdam_client/BillingPdamClientApplication.java
@@ -0,0 +1,13 @@
+package com.charles.billing_pdam_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingPdamClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingPdamClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charles/billing_pdam_client/BillingPdamClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.billing_pdam_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingPdamClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
