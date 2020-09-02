517fc2684d48c805b17552c57b14433dcb3cc02c
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 13:04:14 +0700

    [Charles] register billing_pdam_client into eureka server

--- src/main/java/com/charles/billing_pdam_client/BillingPdamClientApplication.java
+++ src/main/java/com/charles/billing_pdam_client/BillingPdamClientApplication.java
@@ -2,8 +2,10 @@ package com.charles.billing_pdam_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BillingPdamClientApplication {
 
 	public static void main(String[] args) {
