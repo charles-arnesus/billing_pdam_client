42ee59d0b7587617b59c21c8aa702c5b75400c76
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-03 10:32:02 +0700

    [Charles] try to comment test

--- src/test/java/com/charles/billing_pdam_client/BillingPdamClientApplicationTests.java
+++ src/test/java/com/charles/billing_pdam_client/BillingPdamClientApplicationTests.java
@@ -1,13 +1,14 @@
 package com.charles.billing_pdam_client;
 
+import com.charles.billing_pdam_client.controllers.BillController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BillingPdamClientApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class BillingPdamClientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
